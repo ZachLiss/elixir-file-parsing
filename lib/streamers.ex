c82c6986ca0a20a599b6005085a91b6b455876f4
Zach Liss <zach.liss@dockyard.com>
2019-02-15 17:13:58 -0500

    tabs -> spaces

--- lib/streamers.ex
+++ lib/streamers.ex
@@ -4,71 +4,71 @@ defmodule Streamers do
   """
 
   @doc """
-		Find streaming index file in the given directory.
-		"""
+    Find streaming index file in the given directory.
+    """
   def find_index(directory) do
-		files = Path.join(directory, "*.m3u8")
-		Enum.find Path.wildcard(files), &is_index?(&1)
+    files = Path.join(directory, "*.m3u8")
+    Enum.find Path.wildcard(files), &is_index?(&1)
   end
 
-	def is_index?(file) do
-		File.open! file, fn(pid) ->
-			IO.read(pid, 25) == "#EXTM3U\n#EXT-X-STREAM-INF"
-		end
-	end
+  def is_index?(file) do
+    File.open! file, fn(pid) ->
+      IO.read(pid, 25) == "#EXTM3U\n#EXT-X-STREAM-INF"
+    end
+  end
 
   @doc """
-		Extract list of of stream files for given index file.
-		"""
-	def extract_m3u8(index_file) do
-		File.open! index_file, fn(pid) ->
-			# Discards #EXTM3U
-			IO.read(pid, :line)
-			do_extract_m3u8(pid, Path.dirname(index_file), [])
-		end
-	end
+    Extract list of of stream files for given index file.
+    """
+  def extract_m3u8(index_file) do
+    File.open! index_file, fn(pid) ->
+      # Discards #EXTM3U
+      IO.read(pid, :line)
+      do_extract_m3u8(pid, Path.dirname(index_file), [])
+    end
+  end
 
-	defp do_extract_m3u8(pid, dir, acc) do
-		case IO.read(pid, :line) do
-			:eof -> Enum.reverse(acc)
-			stream_inf ->
-				file_name = IO.read(pid, :line)
-				do_extract_m3u8(pid, dir, stream_inf, file_name, acc)
-		end
-	end
+  defp do_extract_m3u8(pid, dir, acc) do
+    case IO.read(pid, :line) do
+      :eof -> Enum.reverse(acc)
+      stream_inf ->
+        file_name = IO.read(pid, :line)
+        do_extract_m3u8(pid, dir, stream_inf, file_name, acc)
+    end
+  end
 
-	defp do_extract_m3u8(pid, dir, stream_inf, file_name, acc) do
-		# #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=110000
-		<< "#EXT-X-STREAM-INF:PROGRAM-ID=", program_id, ",BANDWIDTH=", bandwidth :: binary >> = stream_inf
-		program_id = String.to_integer(<<program_id>>)
-		{ bandwidth, _trash } = Integer.parse(bandwidth)
-		path = Path.join(dir, String.trim(file_name, "\n"))
-		record = %{program_id: program_id, path: path, bandwidth: bandwidth, ts_files: []}
-		do_extract_m3u8(pid, dir, [record|acc])
-	end
+  defp do_extract_m3u8(pid, dir, stream_inf, file_name, acc) do
+    # #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=110000
+    << "#EXT-X-STREAM-INF:PROGRAM-ID=", program_id, ",BANDWIDTH=", bandwidth :: binary >> = stream_inf
+    program_id = String.to_integer(<<program_id>>)
+    { bandwidth, _trash } = Integer.parse(bandwidth)
+    path = Path.join(dir, String.trim(file_name, "\n"))
+    record = %{program_id: program_id, path: path, bandwidth: bandwidth, ts_files: []}
+    do_extract_m3u8(pid, dir, [record|acc])
+  end
 
-	@doc"""
-	  Process M3U8 records to get ts_files
-	  """
-	def process_m3u8(m3u8s) do
-		Enum.map m3u8s, &do_process_m3u8(&1)
-	end
+  @doc"""
+    Process M3U8 records to get ts_files
+    """
+  def process_m3u8(m3u8s) do
+    Enum.map m3u8s, &do_process_m3u8(&1)
+  end
 
-	defp do_process_m3u8(%{ path: path } = m3u8) do
-		File.open! path, fn(pid) ->
-			# discard first 2 lines
-			IO.read(pid, :line)
-			IO.read(pid, :line)
-			%{m3u8 | ts_files: do_process_m3u8(pid, [])}
-		end
-	end
+  defp do_process_m3u8(%{ path: path } = m3u8) do
+    File.open! path, fn(pid) ->
+      # discard first 2 lines
+      IO.read(pid, :line)
+      IO.read(pid, :line)
+      %{m3u8 | ts_files: do_process_m3u8(pid, [])}
+    end
+  end
 
-	defp do_process_m3u8(pid, acc) do
-		case IO.read(pid, :line) do
-			"#EXT-X-ENDLIST\n" -> Enum.reverse(acc)
-			_extinf ->
-				file = IO.read(pid, :line) |> String.trim("\n")
-				do_process_m3u8(pid, [file|acc])
-		end
-	end
+  defp do_process_m3u8(pid, acc) do
+    case IO.read(pid, :line) do
+      "#EXT-X-ENDLIST\n" -> Enum.reverse(acc)
+      _extinf ->
+        file = IO.read(pid, :line) |> String.trim("\n")
+        do_process_m3u8(pid, [file|acc])
+    end
+  end
 end
